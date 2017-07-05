08dcce535e5e8c290d894dca3e8633d6052a494a
Caio Cesar <caiopegoraro@hotmail.com>
2017-07-04 23:18:39 -0300

    Trabalho 1 : implementação do método simplex em C#

---
+++ trab1Simplex/trab1Simplex/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace trab1Simplex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpRestricoes = new System.Windows.Forms.GroupBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtNvariaveis = new System.Windows.Forms.TextBox();
+            this.txtNrestricoes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inicialização";
+            // 
+            // grpRestricoes
+            // 
+            this.grpRestricoes.Location = new System.Drawing.Point(28, 120);
+            this.grpRestricoes.Name = "grpRestricoes";
+            this.grpRestricoes.Size = new System.Drawing.Size(440, 209);
+            this.grpRestricoes.TabIndex = 3;
+            this.grpRestricoes.TabStop = false;
+            this.grpRestricoes.Text = "Restrições:";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(196, 37);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 48);
+            this.btnAdicionar.TabIndex = 5;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(28, 335);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(440, 30);
+            this.btnVerificar.TabIndex = 8;
+            this.btnVerificar.Text = "Verificar  e resolver problema";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtNvariaveis
+            // 
+            this.txtNvariaveis.Location = new System.Drawing.Point(126, 37);
+            this.txtNvariaveis.Name = "txtNvariaveis";
+            this.txtNvariaveis.Size = new System.Drawing.Size(64, 20);
+            this.txtNvariaveis.TabIndex = 9;
+            // 
+            // txtNrestricoes
+            // 
+            this.txtNrestricoes.Location = new System.Drawing.Point(126, 63);
+            this.txtNrestricoes.Name = "txtNrestricoes";
+            this.txtNrestricoes.Size = new System.Drawing.Size(64, 20);
+            this.txtNrestricoes.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Número incognitas:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Número restrições:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Max Z = ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(31, 371);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(437, 147);
+            this.listBox1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 538);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Resultados:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(99, 538);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 17;
+            this.lblResultado.Text = "label6";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(31, 579);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(437, 43);
+            this.listBox2.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 577);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNrestricoes);
+            this.Controls.Add(this.txtNvariaveis);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.grpRestricoes);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Trabalho 1 - Método Simplex";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpRestricoes;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtNvariaveis;
+        private System.Windows.Forms.TextBox txtNrestricoes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ trab1Simplex/trab1Simplex/Form1.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trab1Simplex
+{
+    public partial class Form1 : Form
+    {
+        List<String> listaRestricoes = new List<String>();
+
+        int contador = 1;
+        TextBox[] txtRestricoes;
+        TextBox[] txtResultados;
+        TextBox[] txtLinhaZero;
+
+        Label[] labels, labels2, labels3, labels4;
+        int n = 3;
+
+        int nRestricoes;
+        int nVariaveis;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+
+            btnAdicionar.Enabled = false;
+            
+            /*
+            String txt_temp1 = this.txtEquacao.Text.ToString();
+            String txt_temp2 = this.txtResultado.Text.ToString();
+
+            String txt_temp3 = txt_temp1 + txt_temp2;
+
+            listaRestricoes.Add(txt_temp3);
+            txtEquacao.Text = "";
+            txtResultado.Text = "";
+
+            txtRestricoes.AppendText("\n" + txt_temp1 + " <= " + txt_temp2 + "\n");
+            */
+
+            nRestricoes = int.Parse(txtNrestricoes.Text);
+            nVariaveis = int.Parse(txtNvariaveis.Text);
+
+            txtNrestricoes.ReadOnly = true;
+            txtNvariaveis.ReadOnly = true;
+
+            int x = 50;
+            int y = 150;
+            n = nVariaveis * nRestricoes;
+
+            txtRestricoes = new TextBox[n];
+            txtResultados = new TextBox[nRestricoes];
+            txtLinhaZero = new TextBox[nVariaveis];
+
+            labels = new Label[n];
+            labels2 = new Label[n];
+            labels3 = new Label[n];
+            labels4 = new Label[nVariaveis];
+
+            for (int i = 0; i < nRestricoes; i++)
+            {
+                txtResultados[i] = new TextBox();
+                labels[i] = new Label();
+                labels3[i] = new Label();
+
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                txtRestricoes[j] = new TextBox();
+                labels2[j] = new Label();
+            }
+
+            for (int k = 0; k < nVariaveis; k++)
+            {
+                txtLinhaZero[k] = new TextBox();
+                labels4[k] = new Label();
+            }
+
+            n = 80;
+
+            for (int k = 0; k < nVariaveis; k++)
+            {
+                this.Controls.Add(txtLinhaZero[k]);
+                txtLinhaZero[k].Location = new Point(n, 92);
+                txtLinhaZero[k].Size = new System.Drawing.Size(30, 30);
+                txtLinhaZero[k].BringToFront();
+                n += 30;
+
+                this.Controls.Add(labels4[k]);
+                labels4[k].Location = new Point(n, 95);
+                labels4[k].Text = "X" + k.ToString();
+                labels4[k].BringToFront();
+
+                n += 30;
+
+            }
+
+            n = 0;
+
+            for (int i = 0; i < nRestricoes; i++)
+            {
+                    this.Controls.Add(labels[i]);
+                    labels[i].Location = new Point(x, y);
+                    labels[i].Text = i.ToString();
+                    labels[i].BringToFront();
+                    x += 30;
+
+                int tmp = 0;
+
+                for (int j = 0; j < nVariaveis; j++)
+                {
+                    
+                    this.Controls.Add(txtRestricoes[n]);
+                    y -= 2;
+                    txtRestricoes[n].Location = new Point(x, y);
+                    txtRestricoes[n].BringToFront();
+                    txtRestricoes[n].Text = "0";
+                    txtRestricoes[n].Size = new System.Drawing.Size(30, 30);
+                    y += 2;
+
+                    x += 30;
+                    this.Controls.Add(labels2[n]);
+                    labels2[n].Location = new Point(x, y);
+                    labels2[n].Text = "X" + tmp.ToString();
+                    labels2[n].BringToFront();
+
+                    tmp++;
+                    if (tmp == nVariaveis)
+                        tmp = 0;
+
+                    x += 45;
+                    n++;
+                }
+
+                x -= 10;
+
+                this.Controls.Add(labels3[i]);
+                labels3[i].Location = new Point(x, y);
+                labels3[i].Text = "<=";
+                labels3[i].BringToFront();
+
+                x += 20;
+
+                y -= 2;
+                this.Controls.Add(txtResultados[i]);
+                txtResultados[i].Location = new Point(x, y);
+                txtResultados[i].Size = new System.Drawing.Size(30, 24);
+                txtResultados[i].BringToFront();
+                y += 2;
+
+                x = 50;
+                y += 30;
+
+            }
+
+            //macro para teste rapido
+/*
+            txtResultados[0].Text = "3";
+            txtResultados[1].Text = "4";
+            txtResultados[2].Text = "9";
+
+            txtLinhaZero[0].Text = "5";
+            txtLinhaZero[1].Text = "2";
+
+            txtRestricoes[0].Text = "1";
+            txtRestricoes[1].Text = "0";
+            txtRestricoes[2].Text = "0";
+            txtRestricoes[3].Text = "1";
+            txtRestricoes[4].Text = "1";
+            txtRestricoes[5].Text = "2";
+            */
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            //montar a matriz para aplicar o método simplex
+
+            //precisa: - adicionar espaço para variaveis basicas (novas colunas)
+
+            int nVariaveisFolga = nRestricoes;
+            int nColunas = nVariaveis + nVariaveisFolga + 2; //+1 da variavel Z e +1 da constante
+            int nLinhas = nRestricoes + 1; //+1 da linha zero
+
+            float[,] matrizSimplex;
+            matrizSimplex = new float[nLinhas, nColunas];
+
+            int[] varBase;
+            varBase = new int[nVariaveisFolga];
+
+            for (int i = 0; i < nVariaveisFolga; i++)
+            {
+                varBase[i] = nVariaveis + i;
+            }
+
+            //preencher matriz simplex
+
+            //linha zero
+            for (int i = 0; i < nLinhas; i++)
+            {
+                if (i == 0)
+                    matrizSimplex[0, i] = 1;
+                else if (i < nVariaveis+1)
+                    matrizSimplex[0, i] = (-1)*int.Parse(txtLinhaZero[i-1].Text); //-1 para deixar na forma canonica: Z - aX0 - bX1 = 0
+                else
+                    matrizSimplex[0, i] = 0;
+            }
+            
+            //preencher variaveis nao basicas
+             int k = 0;
+                for (int i = 1; i < nLinhas; i++)
+                {
+                    for (int coluna = 1; coluna <= nVariaveis; coluna++)
+                    {
+                        matrizSimplex[i, coluna] = int.Parse(txtRestricoes[k].Text);
+                        k++;
+                    }
+                    
+                }
+
+            int inX = 1;
+            int inY = nVariaveis + 1;
+            //preencher variáveis básicas
+            for (int i = 1; i < nLinhas; i++)
+            {
+                for (int j = nVariaveis+1; j < nColunas; j++)
+                {
+                    if (j - i == 2)
+                    {
+                        matrizSimplex[i, j] = 1; //marca o indice da variavel basica como 1
+                    }
+                }
+            }
+
+
+            //preencher coluna constantes
+             for (int i = 1; i < nLinhas; i++)
+             {
+                matrizSimplex[i, nColunas-1] = int.Parse(txtResultados[i-1].Text);
+             }
+                 
+            String temp = "";
+            for (int i = 0; i < nLinhas; i++)
+            {
+                for (int j = 0; j < nColunas; j++)
+                {
+                    temp += matrizSimplex[i,j].ToString() + " ";
+                }
+                listBox1.Items.Add(temp);
+                temp = "";
+            }
+
+            //Fim etapa de input dos dados do problema
+
+            //inicio algoritmo de resolução simplex
+
+            //matrizSimplex[i,j]
+            //nColunas,nLinhas
+            //varBase[k] : variaveis base
+
+            int terminou = 0;
+            int colunaPivo = 1, linhaPivo = 1;
+            float numeroPivo = 1;
+
+            while (terminou==0)
+            {
+                //determinar coluna pivo achando o coeficiente negativo na linha zero
+                int achou = 0;
+                for (int i = 0; i < nColunas; i++)
+                {
+                    if (matrizSimplex[0, i] < 0)
+                    {
+                        achou = 1;
+                        colunaPivo = i;
+                        break;//primeiro negativo
+                    }
+                }
+                if (achou == 0)
+                {
+                    terminou = 1;
+                    break;
+                }
+
+                //determinar a linha pivo achando a menor razao positiva entre cte e numero na coluna pivo
+                float razaoTemp=100000,razaoTemp2=10000;
+                numeroPivo = 10000;
+
+                for (int i = 1; i < nLinhas; i++)
+                {
+                    
+                    if (matrizSimplex[i, colunaPivo] == 0)
+                        razaoTemp = 99999999;
+                    else
+                        razaoTemp = matrizSimplex[i, nColunas - 1] / matrizSimplex[i, colunaPivo];
+                    if ((razaoTemp > 0) && (razaoTemp < razaoTemp2))
+                    {
+                        razaoTemp2 = razaoTemp;
+                        linhaPivo = i;
+                    }
+                }
+                numeroPivo = matrizSimplex[linhaPivo, colunaPivo];
+
+                temp = "=== Analise Pivo ===";
+                listBox1.Items.Add(temp);
+                temp = "";
+
+                temp += "linhaP: " + linhaPivo.ToString() + " colunaP: " + colunaPivo.ToString() + " nPivo: " + numeroPivo.ToString();
+                listBox1.Items.Add(temp);
+
+                //determinado coluna, linha e numero pivo
+                
+                //editar base: sai o elemento da linha pivo, entra o elemento da coluna pivo
+                varBase[linhaPivo-1] = colunaPivo-1;
+
+                //gerar nova linha pivo
+                for (int i = 0; i < nColunas; i++)
+                {
+                    matrizSimplex[linhaPivo, i] = matrizSimplex[linhaPivo, i] / numeroPivo;
+                }
+
+                //gerar novas linhas
+                float coefPivo;
+
+                for (int i = 0; i < nLinhas; i++)
+                {
+                    if (i != linhaPivo)
+                    {
+                        coefPivo = matrizSimplex[i, colunaPivo];
+                        for (int j = 0; j < nColunas; j++)
+                        {
+                            matrizSimplex[i, j] = matrizSimplex[i, j] - coefPivo * matrizSimplex[linhaPivo, j];
+                        }
+                    }
+                } 
+
+                temp = "=== " + contador + " interacao Simplex ===";
+                contador++;
+                listBox1.Items.Add(temp);
+                temp = "";
+
+                for (int i = 0; i < nLinhas; i++)
+                {
+                    for (int j = 0; j < nColunas; j++)
+                    {
+                        temp += matrizSimplex[i, j].ToString() + " ";
+                    }
+                    listBox1.Items.Add(temp);
+                    temp = "";
+                }
+            }
+
+            //terminou o algoritmo: precisa exibir os resultados
+
+            String[] resultado = new String[1+nVariaveis+nVariaveisFolga];
+
+            Label[] lblResultados = new Label[1+nVariaveis+nVariaveisFolga];
+
+            for (int i = 0; i < nVariaveisFolga; i++)
+            {
+                resultado[i]= "X" + varBase[i].ToString() + " = " + matrizSimplex[i + 1, nColunas - 1].ToString();
+            }
+
+
+            int qntVar = nVariaveisFolga + nVariaveis;
+            int ktmp = 0;
+
+            for (int i = 0; i < qntVar; i++)
+            {
+                int achou = 0;
+                for (int j = 0; j < nVariaveisFolga; j++)
+                {
+                    if (varBase[j] == i)
+                        achou = 1;
+                }
+
+                if (achou == 0)
+                {
+                    resultado[nVariaveisFolga+ktmp] = "X" + i.ToString() + " = " + "0";
+                    ktmp++;
+                }
+
+            }
+            /*
+            for (int i = 0; i < nVariaveisFolga; i++)
+            {
+                listBox2.Items.Add(varBase[i].ToString());
+            }
+            */
+            //exibir resultados na label
+            //lblResultado.Text ="["+resultado[0] +"] " + "[" + resultado[1] + "] " + "[" + resultado[2] + "] " + "[" + resultado[3] + "] ";
+            String resTemp = "";
+
+            for (int i = 0; i < nVariaveisFolga+nVariaveis; i++)
+            {
+                resTemp += "[" + resultado[i] + "] ";
+            }
+            lblResultado.Text = resTemp + "[Z=" + matrizSimplex[0,nColunas-1] + "] ";
+        }
+    }
+}

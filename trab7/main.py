849064e5eb50af97aa156acd2597ad50220c9c4e
Raphael Adamski <raphaeladamski@hotmail.com>
2017-07-12 23:21:50 -0300

    calc mean, scale, covariance and eigenValues and Vec

--- trab7/main.py
+++ trab7/main.py
@@ -1,9 +1,20 @@
 import numpy as np
 import networkx as nx
+import sys
+from numpy import linalg as LA
 
-A = np.loadtxt('matriz.txt')
-G = nx.from_numpy_matrix(A)
+if len(sys.argv) > 1:
+    arq = sys.argv[1]
+else:
+    print("Enter a filename")
+    arq = input()
+print(arq)
+data = np.loadtxt(arq, delimiter=',')
+mean = data.mean(axis=0, keepdims=True);
+print(mean)
+scaledData = data - mean
+print(scaledData.mean(axis=0))
+covMatrix = np.cov(scaledData)
+eigenValues, eigenVectors = LA.eig(covMatrix)
+print("teste")
 
-# liste de vértices e arestas com pesos
-vertices = G.nodes()
-arestas = G.edges(data=True)
